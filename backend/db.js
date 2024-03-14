0e147be1dfe8474e8589d2a026568ee417a8b703
hariom <hk524569@gmail.com>
2024-03-14 14:07:01 +0530

    connected db

---
+++ backend/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export async function connecteMongoDb(url){
+    mongoose.connect(url);
+}
+
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {connecteMongoDb} from './db.js'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+
 const app= express();
 app.use(bodyParser.json())
+
+connecteMongoDb(process.env.MOGOLOCAURL)
+.then(()=>{
+    console.log('connected to mongodb')
+})
+.catch(err=>console.log('error connecting',err))
 
 app.get('/',(req,res)=>{
     return res.send("<h1>hello</h1>")
